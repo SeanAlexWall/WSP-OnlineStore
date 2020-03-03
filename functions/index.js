3dbaf0f07a18250efc876c1e3aa5f3da235c20fa
SeanAlexWall <seanalexwall@gmail.com>
2020-03-02 22:47:35 -0600

    learning about backend development

--- functions/index.js
+++ functions/index.js
@@ -2,13 +2,17 @@ const functions = require('firebase-functions');
 const express = require('express');
 const app = express();
 
-
 exports.httpreq = functions.https.onRequest(app);
 
+app.use(express.urlencoded({extended: false}))
+app.use('/public', express.static(__dirname + '/static'));
 
-app.get('/', (req, res) => {
-    res.send('<h1>My Store (bnd) </h1>');
-});
+//set template engine
+app.set('view engine', 'ejs');
+app.set('views', './ejsviews');
+
+
+
 
 function frontendHandler( request, response){
     response.sendFile( __dirname + '/prodadmin/prodadmin.html');
@@ -19,3 +23,19 @@ app.get('/home', frontendHandler);
 app.get('/add', frontendHandler);
 app.get('/show', frontendHandler);
 app.get('/profile', frontendHandler);
+
+app.get('/testLogin', (req, res) => {
+    res.sendFile(__dirname + '/static/html/login.html')
+})
+
+app.post('/testSignIn', (req, res)=>{
+    const email = req.body.email;
+    const password = req.body.pass;
+
+    const obj = {email, password};
+    res.render('home', obj)
+})
+
+app.get('/', (req, res) => {
+    res.send('<h1>My Store (bnd) </h1>');
+});
